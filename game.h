0f81436bbcc8b56617fa23ffa953d328c922f704
Harrison Kight <harrison.c.kight@icloud.com>
2016-04-20 09:30:51 -0400

    Added files via upload

---
+++ game.h
@@ -0,0 +1,26 @@
+#ifndef game_h
+#define game_h
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <ncurses.h>
+
+using namespace std;
+
+class Dork {
+private:
+    string name;
+    string level;
+    int energy;
+    int steps;
+    
+public:
+    Dork();
+    Dork(string in_name, string in_level, int in_energy, int in_steps);
+    
+    void Display(std::ifstream& inFile);
+};
+
+#endif
