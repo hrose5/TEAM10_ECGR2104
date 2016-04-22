e2c8388e3c68e3f5d66445e67d72e74d01eebcd7
Harrison Kight <harrison.c.kight@icloud.com>
2016-04-22 14:23:18 -0400

    Update game.h

--- game.h
+++ game.h
@@ -11,18 +11,17 @@ using namespace std;
 
 class Dork {
 private:
-    string name;
-    string level;
-    
-     int energy;
+    int energy;
     int steps;
+    
+    string name;
+    string map;
     
 public:
     Dork();
     Dork(string in_name, string in_level, int in_energy, int in_steps);
-
-    void clearDisplay(int num);
     
+    void clearDisplay(int num);
 };
 
 #endif
